bbdfa19bdff3a5370b3319459afd12ec57c62332
Greg McClure <gmcclure@gmail.com>
2012-09-08 12:30:13 -0700

    Added Makefile for testing. Accepted.

--- uva/10137-the_trip/main.c
+++ uva/10137-the_trip/main.c
@@ -5,7 +5,7 @@
 int main(int argc, char const *argv[])
 {
     char cents[100], line[100];
-    int *amts, avg=0, i, s, total=0, xchg=0;
+    int avg=0, i, p, s, total=0, xchg=0;
 
     while(1) {
         scanf("%d", &s);
@@ -27,32 +27,24 @@ int main(int argc, char const *argv[])
         }
 
         avg = total / s;
+        p = total % s;
 
         for(i=0; i<s; ++i) {
-            if(amts[i] < avg) xchg += avg - amts[i];
+            if(amts[i] < avg) {
+                xchg += avg - amts[i];
+                continue;
+            }
+
+            if((amts[i] > avg) && p) p--;
         }
 
-        printf("xchg: %d\n", xchg);
+        xchg += p;
+
+        printf("$%d.%02d\n", xchg/100, xchg%100);
 
         free(amts);
-        total=0, xchg=0;
+        p=0, total=0, xchg=0;
     }
 
     return 0;
 }
-
-/*
- * Correct output for input.txt:
- * $10.00
- * $11.99
- * $1.10
- * $2407.09
- * $5.00
- * $0.00
- * $0.01
- * $3991.11
- * $0.01
- * $2.25
- * $4.73
- * $0.02
-*/
