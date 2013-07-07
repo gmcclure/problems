dd4c2bbea190ee0b6f2cdcbad98358c9decc454d
Greg McClure <gmcclure@gmail.com>
2013-07-07 11:02:04 -0700

    Accepted.

--- uva/706-lcd_display/main.c
+++ uva/706-lcd_display/main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define LCD_ROWS 5
 #define LCD_COLS 3
@@ -17,32 +18,46 @@ int main(int argc, char const *argv[])
                    0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, /* 8 */
                    0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, /* 9 */ };
 
-    int i, j, k=0, len=0, size=0;
-    char num[20];
+    int i, j, k=0, len=0, m, n, size=0, t, tc;
+    char num[20], p;
 
     scanf("%d %s", &size, num);
 
-    if(size==0) exit(0);
-
-    len = sizeof(num)/sizeof(char);
-    printf("len: %d\n", len);
-
-    for (i = 0; i < LCD_ROWS; i++) {
-
-    }
-
-    for(k = 0; k < len; k++) {
+    while (size) {
         for(i=0; i<LCD_ROWS; ++i) {
-            for(j=0; j<LCD_COLS; ++j) {
-                if (mtrx[(num[k]-'0')*15+i*3+j]) {
-                    if (j%2) {
-                        printf("|");
-                    } else {
-                        printf("-");
-                    }
-                }
+
+            if(size==0) exit(0);
+            len = strnlen(num, 20);
+
+            if(i%2) {
+                p='|';
+                t=size; 
+                tc=1;
+            } else {
+                p='-';
+                t=1;
+                tc=size;
             }
-            printf("\n");
+
+            for (m=0; m<t; m++) {
+                for(k=0; k<len; k++) {
+                    for(j=0; j<LCD_COLS; ++j) {
+                        if (mtrx[(num[k]-'0')*15+i*3+j]) {
+                            for(n=0; n<tc; n++) printf("%c", p);
+                        } else {
+                            if(j%2)
+                                for(n=0; n<size; n++) printf(" ");
+                            else
+                                printf(" ");
+                        }
+                    }
+                    if(k < len-1) printf(" ");
+                }
+                printf("\n");
+            }
         }
+        printf("\n");
+
+        scanf("%d %s", &size, num);
     }
 }
