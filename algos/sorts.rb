dac2aa8ea223a664105b366cc0f81effd3e477b8
Greg McClure <gmcclure@gmail.com>
2014-02-14 21:42:05 -0800

    Added swapped flag to bubble sort.

--- algos/sorts.rb
+++ algos/sorts.rb
@@ -1,13 +1,20 @@
+##
 # sorts.rb
+# A module of the usual sorting suspects.
 
 module Sorts
 
   def self.bubble_sort(c)
     0.upto(c.size-1) do |i|
+      swapped = false
       (c.size-1).downto(i+1) do |j|
         p = block_given? ? yield(c[j], c[j-1]) : c[j] <=> c[j-1]
-        c[j-1], c[j] = c[j], c[j-1] if p == -1
+        if p == -1
+          c[j-1], c[j] = c[j], c[j-1]
+          swapped = true
+        end
       end
+      break if not swapped
     end
     c
   end
